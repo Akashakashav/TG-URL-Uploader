8c5fe327573ac835a5881563cc6e84a2117c48ca
NIKHIL5757H <65679470+NIKHIL5757H@users.noreply.github.com>
2020-12-11 16:01:33 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -63,7 +63,6 @@ async def get_me_info(bot, update):
 async def start(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/start")
-
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.first_name),
